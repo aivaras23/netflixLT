e2c8f01f262d71e3ae33694e51b8995e4b4e0487
aivaras23 <aivaraspal1@gmail.com>
2024-04-26 13:47:04 +0300

    update7

--- app.js
+++ app.js
@@ -28,4 +28,23 @@ document.addEventListener('DOMContentLoaded', function () {
         return regex.test(email);
     }
 
+
+
+    const contentTexts = document.querySelectorAll(".accordion__content");
+    const clicks = document.querySelectorAll(".click");
+
+
+    for (let i = 0; i < clicks.length; i++) {
+        clicks[i].addEventListener("click", function () {
+
+            if (contentTexts[i].classList.contains("accordion__active")) {
+                clicks[i].setAttribute("src", "open.png");
+            } else {
+                clicks[i].setAttribute("src", "close.png");
+            }
+            contentTexts[i].classList.toggle("accordion__active");
+
+        });
+    }
+
 });

9a8eb4ebf469cdbd139aee28403b2166111846ea
aivaras23 <aivaraspal1@gmail.com>
2024-04-26 13:52:56 +0300

    update8

--- app.js
+++ app.js
@@ -30,21 +30,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    const contentTexts = document.querySelectorAll(".accordion__content");
-    const clicks = document.querySelectorAll(".click");
-
-
-    for (let i = 0; i < clicks.length; i++) {
-        clicks[i].addEventListener("click", function () {
-
-            if (contentTexts[i].classList.contains("accordion__active")) {
-                clicks[i].setAttribute("src", "open.png");
-            } else {
-                clicks[i].setAttribute("src", "close.png");
-            }
-            contentTexts[i].classList.toggle("accordion__active");
-
-        });
-    }
-
 });
+
+
+const contentTexts = document.querySelectorAll(".accordion__content");
+const clicks = document.querySelectorAll(".click");
+
+for (let i = 0; i < clicks.length; i++) {
+    clicks[i].addEventListener("click", function () {
+        alert('abc')
+
+        if (contentTexts[i].classList.contains("accordion__active")) {
+            clicks[i].setAttribute("src", "./images/faq-container/open.png");
+        } else {
+            clicks[i].setAttribute("src", "./images/faq-container/close.png");
+        }
+        contentTexts[i].classList.toggle("accordion__active");
+
+    });
+}
